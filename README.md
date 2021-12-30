943bf65e665fd34f5554291eab14183b98b4b419
Assiya Guenoune <assiyabou8@gmail.com>
2021-12-30 21:08:55 +0100

    la base de projet finale

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Geodesique_visualisation
 visualisation et calcul
+Cette application web vous permet de calculer outputs de la problème directe et inverse . puis, de les visualiser en 3D.
+Nous ésperons que cette App va répondre vos attentes.
+Merci de laisser vos feadback.
--- main/settings.py
+++ main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+    ]
+STATIC_ROOT= os.path.join(BASE_DIR, 'assests')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
