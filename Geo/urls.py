15c8ccca96e0bc6ee43e81db3affa6a25be421f9
Assiya Guenoune <assiyabou8@gmail.com>
2021-12-30 23:54:49 +0100

    First Design

--- Geo/urls.py
+++ Geo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home , name="home")
+  path('', views.home , name="home"),
+  path('Contact', views.contact , name="contact"),
+  path('Direct', views.direct , name="direct"),
 ]
--- Geo/views.py
+++ Geo/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
      return render(request,'Home.html')
+def contact(request):
+    return render(request,'Contact.html')
+def direct(request):
+     return render(request,'direct.html')
